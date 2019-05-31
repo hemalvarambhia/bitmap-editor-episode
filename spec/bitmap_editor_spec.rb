d01b29dfe5051431a96acc4bd5fd4066e24bff29
Hemal Varambhia <h.n.varambhia@gmail.com>
2019-05-31 20:15:33 +0100

    Fake 'til we make - we can create an image painted white that is 1px by 1px.

--- spec/bitmap_editor_spec.rb
+++ spec/bitmap_editor_spec.rb
@@ -1,6 +1,22 @@
-describe '2 + 2' do
+class BitmapEditor
+  def initialize(image)
+    @image = image
+  end
 
-  it 'equals 4' do
-    expect(2 + 2).to eq 4
+  def run(command)
+    @image.create(width: 1, height: 1)
+  end
+end
+
+describe 'Creating an image' do
+  context '1 x 1 image' do
+    let(:image) { double(:image) }
+    let(:bitmap_editor) { BitmapEditor.new(image) }
+
+    it 'creates an image that 1px by 1px, coloured white' do
+      expect(image).to receive(:create).with(width: 1, height: 1)
+
+      bitmap_editor.run('I 1 1')
+    end
   end
 end
