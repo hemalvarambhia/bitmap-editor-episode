aa8f2fe961898b01d1ac905a8299b874723ca281
Hemal Varambhia <h.n.varambhia@gmail.com>
2019-05-31 20:25:39 +0100

    Triangulation - we can create an image of any width as long as height is 1px.

--- spec/bitmap_editor_spec.rb
+++ spec/bitmap_editor_spec.rb
@@ -4,19 +4,29 @@ class BitmapEditor
   end
 
   def run(command)
-    @image.create(width: 1, height: 1)
+    _, *args = command.split
+    width = args[0].to_i
+    @image.create(width: width, height: 1)
   end
 end
 
 describe 'Creating an image' do
-  context '1 x 1 image' do
-    let(:image) { double(:image) }
-    let(:bitmap_editor) { BitmapEditor.new(image) }
+  let(:image) { double(:image) }
+  let(:bitmap_editor) { BitmapEditor.new(image) }
 
-    it 'creates an image that 1px by 1px, coloured white' do
+  context '1 x 1 image' do
+    it 'creates an image that is 1px by 1px, coloured white' do
       expect(image).to receive(:create).with(width: 1, height: 1)
 
       bitmap_editor.run('I 1 1')
+    end
+  end
+
+  context '2 x 1 image' do
+    it 'creates an image that is 2px by 1px, coloured white' do
+      expect(image).to receive(:create).with(width: 2, height: 1)
+
+      bitmap_editor.run('I 2 1')
     end
   end
 end
