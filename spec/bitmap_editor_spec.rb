91549ef10427aa6544a30dc30713ce64df6e3b88
Hemal Varambhia <h.n.varambhia@gmail.com>
2019-06-23 20:23:58 +0100

    Moved the class to its own file in lib. It's big enough that we can do that.

--- spec/bitmap_editor_spec.rb
+++ spec/bitmap_editor_spec.rb
@@ -1,36 +1,5 @@
 require 'ostruct'
-class BitmapEditor
-  WIDTH  = 0.freeze
-  HEIGHT = 1.freeze
-
-  def initialize(image)
-    @image = image
-  end
-
-  def run(command)
-    type, *args = command.split
-    case type
-    when 'I'
-      initialize_image(width: args[WIDTH].to_i, height: args[HEIGHT].to_i)
-    when 'L'
-      x = args[0].to_i
-      y = args[1].to_i
-      colour = args[2]
-      @image.paint_pixel(coordinate: coordinate(x, y), colour: colour)
-    end
-
-  end
-
-  private
-
-  def initialize_image(width:, height:)
-    @image.create(width: width, height: height)
-  end
-
-  def coordinate(x, y)
-    OpenStruct.new(x: x, y: y)
-  end
-end
+require 'bitmap_editor'
 
 describe BitmapEditor do
   let(:image) { double(:image) }
